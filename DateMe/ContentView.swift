f4611959e199b727891323ff1c849caf2238f2c9
johns351 <johns351@coventry.ac.uk>
2020-01-07 10:47:31 +0000

    Initial Commit

---
+++ DateMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DateMe
+//
+//  Created by Tony Hajdini on 07/01/2020.
+//  Copyright © 2020 Tony Hajdini. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
